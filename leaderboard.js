38c0684e53de9f1f25e3a34bb3660fcb75c2a76c
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2018-12-21 19:02:06 +0545

    updated leaderboard

--- leaderboard.js
+++ leaderboard.js
@@ -236,6 +236,5 @@ function responseFromSlackListener(response){
     logger.info("ts:" + JSON.stringify(response.body.ts));
     logger.info("Messages Map:" + JSON.stringify(messages_map[response.body.ts]));*/
 }
-logger.debugger(emitter.removeAllListeners());
 app.listen(config.server.port, config.server.hostname);
 
